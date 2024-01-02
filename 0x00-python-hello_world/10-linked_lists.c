2ff340766b4db0d0caa51228e1912939e2e0ec21
yasabri <yasabri04@gmail.com>
2024-01-02 12:00:45 -0800

    py

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -4,31 +4,31 @@
 
 /**
  * print_listint - prints all elements of a listint_t list
- * @h: pointer to the head of list
- * Return: nods number
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
   const listint_t *current;
-  unsigned int x;
+  unsigned int n;
 
   current = h;
-  x = 0;
+  n = 0;
   while (current != NULL)
     {
-      printf("%i\x", current->x);
+      printf("%i\n", current->n);
       current = current->next;
-      x++;
+      n++;
     }
 
-  return (x);
+  return (n);
 }
 
 /**
- * add_nodeint - inserts a fresh node at the start of a listint_t list
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
- * @head: pointer to a pointer of start, list
- * Return: the address of new element , NULL if it fails
+ * Return: address of the new element or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
@@ -47,7 +47,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 /**
  * free_listint - frees a listint_t list
- * @head: the pointer to the list to be freed
+ * @head: pointer to list to be freed
  * Return: void
  */
 void free_listint(listint_t *head)
