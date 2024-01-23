146901f155c337112d53049240d90c0dfc513d16
yasabri <yasabri04@gmail.com>
2024-01-23 10:50:05 -0800

    class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+    """Specify a class Square."""
+
+
+class Square:
+    """Show off a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+    """specify a class Square."""
+
+
+class Square:
+    """Show off a square."""
+
+    def __init__(self, size):
+        self.__size = size
