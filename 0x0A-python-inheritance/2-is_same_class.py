2d6bfb8c20bcfbe004617e89bb472a0f64b110fb
yasabri <yasabri04@gmail.com>
2024-02-05 12:30:45 -0800

    my

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,7 +2,9 @@
 """Defines a class-checking function."""
 
 def is_same_class(obj, a_class):
+
     """This module contains the function is_same_class"""
+
     if type(obj) == a_class:
         return True
     return False
