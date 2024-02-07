24b3cd497c8551799dd51af361c0835cd82a7b1b
yasabri <yasabri04@gmail.com>
2024-02-07 12:53:55 -0800

    me

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns an object by
-a JSON representation
-"""
+"""Defines a JSON-to-object function."""
 import json
 
 
 def from_json_string(my_str):
-        """determines if an item is a class instance that was inherited
-
-    Args:
-        obj (any): object to check
-        a_class (type): class to match type of obje to.
-    Returns:
-        If obj is an instance of a_class that was inherited: True.
-        If not, then false
-    """
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
+
