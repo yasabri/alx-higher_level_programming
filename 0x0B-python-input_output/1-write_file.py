946378465d448ee94132f3060843176440fead14
yasabri <yasabri04@gmail.com>
2024-02-07 12:46:02 -0800

    me

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""describes a class for inherited lists. MyList"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Deploys sorted printing functionality
-    for the integrated list class."""
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """A list arranged ascendingly can be printed."""
-
+        """Print a list in sorted ascending order."""
         print(sorted(self))
