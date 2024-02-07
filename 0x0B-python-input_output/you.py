67989561ed0c03f10160e0771b839bbd7b151807
yasabri <yasabri04@gmail.com>
2024-02-07 13:50:55 -0800

    me

--- 0x0B-python-input_output/you.py
+++ 0x0B-python-input_output/you.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a JSON file-reading function."""
+"""defines a function for reading JSON files."""
 import json
 
 
 def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
+    """From a JSON source, create a Python object"""
     with open(filename) as f:
         return json.load(f)
