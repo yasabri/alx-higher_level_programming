e7561b72c9ce3abb0768c2ceeade60a9f051bfdd
yasabri <yasabri04@gmail.com>
2024-04-11 08:24:46 -0700

    me

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
-"""  displays every state from the hbtn_0e_0_usa database  """
-import MySQLdb
-import sys
 
+#!/usr/bin/python3
+"""
+displays every state from the hbtn_0e_0_usa database
+
+"""
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("chose * FROM states WHERE name LIKE %s", (match, ))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states""")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
